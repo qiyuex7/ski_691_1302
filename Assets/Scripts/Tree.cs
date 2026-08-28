a5442d18f1995883596242a5d46de720f030048d
qiyuex7 <cmm81390@gmail.com>
2026-08-28 16:27:04 +0700

    Finish Gate

--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -26,6 +26,13 @@ public class Tree : MonoBehaviour
             return;
 
         player.HP -= 15;
+        UIManager.instance.ShowNotiText($"Hurt -15\nHP: {player.HP}");
+
+        if (player.HP <= 0)
+        {
+            player.HP = 0;
+            UIManager.instance.ShowNotiText($"You are dead !!!\nPoints: {player.Point}");
+        }
     }
 
     private void OnCollisionExit(Collision collision)
